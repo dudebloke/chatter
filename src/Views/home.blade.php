7a7b0cb33b9554ee554fc9daf1a4fad4cff08334
Raymonf <Raymonf@users.noreply.github.com>
2016-10-07 15:57:59 -0400

    Update home.blade.php

--- src/Views/home.blade.php
+++ src/Views/home.blade.php
@@ -58,49 +58,54 @@
 				<!-- END SIDEBAR -->
 	    	</div>
 	        <div class="col-md-9 right-column">
-	        	<div class="panel">
-		        	<ul class="discussions">
-		        		@foreach($discussions as $discussion)
-				        	<li>
-				        		<a class="discussion_list" href="/{{ Config::get('chatter.routes.home') }}/{{ Config::get('chatter.routes.discussion') }}/{{ $discussion->category->slug }}/{{ $discussion->slug }}">
-					        		<div class="chatter_avatar">
-					        			@if(Config::get('chatter.user.avatar_image_database_field'))
-					        				
-					        				<?php $db_field = Config::get('chatter.user.avatar_image_database_field'); ?>
-					        				
-					        				<!-- If the user db field contains http:// or https:// we don't need to use the relative path to the image assets -->
-					        				@if( (substr($discussion->user->{$db_field}, 0, 7) == 'http://') || (substr($discussion->user->{$db_field}, 0, 8) == 'https://') )
-					        					<img src="{{ $discussion->user->{$db_field}  }}">
-					        				@else
-					        					<img src="{{ Config::get('chatter.user.relative_url_to_image_assets') . $discussion->user->{$db_field}  }}">
-					        				@endif
-					        			
-					        			@else
-					        				
-					        				<span class="chatter_avatar_circle" style="background-color:#<?= Chatter::stringToColorCode($discussion->user->email) ?>">
-					        					{{ strtoupper(substr($discussion->user->email, 0, 1)) }}
-					        				</span>
-					        				
-					        			@endif
-					        		</div>
+		        @if(count($discussions) < 1)
+			        <p>Welp, guess there aren't any posts here!</p>
+		        @else
+		            <div class="panel">
+			            <ul class="discussions">
 
-					        		<div class="chatter_middle">
-					        			<h3 class="chatter_middle_title">{{ $discussion->title }} <div class="chatter_cat" style="background-color:{{ $discussion->category->color }}">{{ $discussion->category->name }}</div></h3>
-					        			<span class="chatter_middle_details">Posted By: <span data-href="/user">{{ ucfirst($discussion->user->{Config::get('chatter.user.database_field_with_user_name')}) }}</span> {{ \Carbon\Carbon::createFromTimeStamp(strtotime($discussion->created_at))->diffForHumans() }}</span>
-					        			<p>{{ substr(strip_tags($discussion->post[0]->body), 0, 200) }}@if(strlen(strip_tags($discussion->post[0]->body)) > 200){{ '...' }}@endif</p>
-					        		</div>
+			                @foreach($discussions as $discussion)
+					            <li>
+					                <a class="discussion_list" href="/{{ Config::get('chatter.routes.home') }}/{{ Config::get('chatter.routes.discussion') }}/{{ $discussion->category->slug }}/{{ $discussion->slug }}">
+						                <div class="chatter_avatar">
+						                    @if(Config::get('chatter.user.avatar_image_database_field'))
 
-					        		<div class="chatter_right">
-					        			
-					        			<div class="chatter_count"><i class="chatter-bubble"></i> {{ $discussion->postsCount[0]->total }}</div>
-					        		</div>
+						                        <?php $db_field = Config::get('chatter.user.avatar_image_database_field'); ?>
 
-					        		<div class="chatter_clear"></div>
-					        	</a>
-				        	</li>
-			        	@endforeach
-		        	</ul>
-	        	</div>
+						                        <!-- If the user db field contains http:// or https:// we don't need to use the relative path to the image assets -->
+						                        @if( (substr($discussion->user->{$db_field}, 0, 7) == 'http://') || (substr($discussion->user->{$db_field}, 0, 8) == 'https://') )
+						                            <img src="{{ $discussion->user->{$db_field}  }}">
+						                        @else
+						                            <img src="{{ Config::get('chatter.user.relative_url_to_image_assets') . $discussion->user->{$db_field}  }}">
+						                        @endif
+
+						                    @else
+
+						                        <span class="chatter_avatar_circle" style="background-color:#<?= Chatter::stringToColorCode($discussion->user->email) ?>">
+						                            {{ strtoupper(substr($discussion->user->email, 0, 1)) }}
+						                        </span>
+
+						                    @endif
+						                </div>
+
+						                <div class="chatter_middle">
+						                    <h3 class="chatter_middle_title">{{ $discussion->title }} <div class="chatter_cat" style="background-color:{{ $discussion->category->color }}">{{ $discussion->category->name }}</div></h3>
+						                    <span class="chatter_middle_details">Posted By: <span data-href="/user">{{ ucfirst($discussion->user->{Config::get('chatter.user.database_field_with_user_name')}) }}</span> {{ \Carbon\Carbon::createFromTimeStamp(strtotime($discussion->created_at))->diffForHumans() }}</span>
+						                    <p>{{ substr(strip_tags($discussion->post[0]->body), 0, 200) }}@if(strlen(strip_tags($discussion->post[0]->body)) > 200){{ '...' }}@endif</p>
+						                </div>
+
+						                <div class="chatter_right">
+
+						                    <div class="chatter_count"><i class="chatter-bubble"></i> {{ $discussion->postsCount[0]->total }}</div>
+						                </div>
+
+						                <div class="chatter_clear"></div>
+						            </a>
+					            </li>
+				            @endforeach
+			            </ul>
+		            </div>
+		        @endif
 
 	        	<div id="pagination">
 	        		{{ $discussions->links() }}
